3266be8089244f3b4e608ecc26c06835b4ce807f
SantiagoVelasquezMontoya <santiago.velasquez@ias.com.co>
2023-03-02 22:12:09 -0500

    .

--- src/main/java/com/reactive/flux/FluxApplication.java
+++ src/main/java/com/reactive/flux/FluxApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 @FunctionalInterface
 interface Predicate<String> {
@@ -13,11 +18,59 @@ public class FluxApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hellghjfhfgo");
 
 		Predicate<String> myPredicate = (string) -> string.startsWith("S");
+		Mono.just("Santiago").log()
+				.doOnSuccess(System.out::println)
+				.doOnRequest(System.out::println)
+				.doOnError(System.out::println)
+				.subscribe(myPredicate::check);
 
-		Mono.just("Santiago").log().subscribe(person -> System.out.println(person.startsWith("F")));
+		Predicate<Integer> greaterThan50 = n -> n > 50;
+
+		List<Integer> list1 = List.of(12,4,54,6,5,43,534);
+		List<Integer> list2 = List.of(412,4,5,6,51,43,5);
+
+		List<List<Integer>> listList = new ArrayList<>();
+		listList.add(list1);
+		listList.add(list2);
+
+		listList.stream()
+				.flatMap(list -> list.stream().filter(greaterThan50::check))
+				.forEach(System.out::println);
+
+//		Long result = fib(100);
+//		System.out.println("Result: " + result);
+
+		int offset = 1;
+		Stream.iterate(new float[]{0, 1}, n -> new float[]{n[1], n[0] + n[1]})
+				.limit(100 + offset)
+				.map(t -> t[0])
+				.forEach(n -> System.out.print(n + " "));
+
+
+		long sum = Stream.iterate(new long[]{0, 1}, t -> new long[]{t[1], t[0] + t[1]})
+				.limit(100)
+				.map(t -> t[0])
+				.mapToLong(Long::intValue)
+				.sum();
+
+		System.out.println("Total : " + sum);
+
+
+	}
+
+	public static  Long fib(int n) {
+		if(n==0){
+			return 0L;
+		}
+		if(n==1){
+			return 1L;
+		}
+		if(n==2){
+			return 1L;
+		}
+		return fib(n-1)+fib(n-2);
 	}
 
 
